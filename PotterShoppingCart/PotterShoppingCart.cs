6bb1ef54e906f1ace345a0e1b2d2b19962ac9fa8
Yen-Ju <winterer0412@gmail.com>
2015-10-26 21:15:11 +0800

    Finished scenario 1

--- PotterShoppingCart/PotterShoppingCart.cs
+++ PotterShoppingCart/PotterShoppingCart.cs
@@ -7,19 +7,25 @@ namespace PotterShoppingCart
 {
     public class PotterShoppingCart
     {
+        List<PotterShoppingCartItem> cartItemsList = new List<PotterShoppingCartItem>();
+
         public void AddToCart(PotterShoppingCartItem shoppingCartItem)
         {
-            throw new NotImplementedException();
+            cartItemsList.Add(shoppingCartItem);
         }
 
         public int CheckOut()
         {
-            throw new NotImplementedException();
+            int totalCost = 0;
+            foreach (PotterShoppingCartItem item in cartItemsList)
+                totalCost += item.Quantity * item.Price;
+            return totalCost;
         }
     }
 
     public class PotterShoppingCartItem
     {
+        internal int Price = 100;
         public int Quantity { get; set; }
         public HarryPotter Volumn { get; set; }
     }
