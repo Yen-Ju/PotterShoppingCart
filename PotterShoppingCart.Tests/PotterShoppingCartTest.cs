52aad756c1f7efd8d0dc2355eaf3682e36f22684
Yen-Ju <winterer0412@gmail.com>
2015-10-26 20:52:22 +0800

    Add scenario 1 test case

--- PotterShoppingCart.Tests/PotterShoppingCartTest.cs
+++ PotterShoppingCart.Tests/PotterShoppingCartTest.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using PotterShoppingCart;
 
 namespace PotterShoppingCart.Tests
 {
     [TestClass]
     public class PotterShoppingCartTest
     {
+        [TestMethod]
+        public void Buy_1_First_Vol_Cost_100()
+        {
+            //arrange
+            var target = new PotterShoppingCart();
+            var shoppingCartItem = new PotterShoppingCartItem() {
+                Volumn = HarryPotter.VOL_1,
+                Quantity = 1
+            };
+            var expectedCost = 100;
+
+            //act
+            int actualCost;
+            target.AddToCart(shoppingCartItem);
+            actualCost = target.CheckOut();
+
+            //assert
+            Assert.AreEqual(expectedCost, actualCost);
+        }
     }
 }
---
+++ PotterShoppingCart/PotterShoppingCart.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterShoppingCart
+{
+    public class PotterShoppingCart
+    {
+        public void AddToCart(PotterShoppingCartItem shoppingCartItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CheckOut()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class PotterShoppingCartItem
+    {
+        public int Quantity { get; set; }
+        public HarryPotter Volumn { get; set; }
+    }
+
+    public enum HarryPotter
+    {
+        VOL_1
+    }
+}
