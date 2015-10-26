12fe8d6d2857289caa5f207826b99389a82a8156
Yen-Ju <winterer0412@gmail.com>
2015-10-26 22:05:42 +0800

    Add scenario 6 test case

--- PotterShoppingCart.Tests/PotterShoppingCartTest.cs
+++ PotterShoppingCart.Tests/PotterShoppingCartTest.cs
@@ -125,6 +125,7 @@ namespace PotterShoppingCart.Tests
             //assert
             Assert.AreEqual(expectedCost, actualCost);
         }
+
         [TestMethod]
         public void Buy_1_Of_Each_Vol_1_to_5_Cost_375()
         {
@@ -164,6 +165,39 @@ namespace PotterShoppingCart.Tests
             target.AddToCart(shoppingCartItem3);
             target.AddToCart(shoppingCartItem4);
             target.AddToCart(shoppingCartItem5);
+            actualCost = target.CheckOut();
+
+            //assert
+            Assert.AreEqual(expectedCost, actualCost);
+        }
+
+        [TestMethod]
+        public void Buy_1_Of_Each_Vol_1_and_2_and_2_Vol_3_Cost_370()
+        {
+            //arrange
+            var target = new PotterShoppingCart();
+            var shoppingCartItem1 = new PotterShoppingCartItem()
+            {
+                Volumn = HarryPotter.VOL_1,
+                Quantity = 1
+            };
+            var shoppingCartItem2 = new PotterShoppingCartItem()
+            {
+                Volumn = HarryPotter.VOL_2,
+                Quantity = 1
+            };
+            var shoppingCartItem3 = new PotterShoppingCartItem()
+            {
+                Volumn = HarryPotter.VOL_3,
+                Quantity = 2
+            };
+            var expectedCost = 370;
+
+            //act
+            int actualCost;
+            target.AddToCart(shoppingCartItem1);
+            target.AddToCart(shoppingCartItem2);
+            target.AddToCart(shoppingCartItem3);
             actualCost = target.CheckOut();
 
             //assert
